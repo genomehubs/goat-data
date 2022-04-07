f34ff0fcfbbbcd20c4b562358cdec7782aec44fe
Sujai Kumar <sujaikumar@users.noreply.github.com>
2022-04-07 08:05:13 +0100

    Update get_ena_taxonomy_extra.bash

--- scripts/get_ena_taxonomy_extra.bash
+++ scripts/get_ena_taxonomy_extra.bash
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-mkdir /tmp/ena-taxonomy
-cd /tmp/ena-taxonomy
-wget ftp://ftp.ebi.ac.uk/pub/databases/ena/taxonomy/taxonomy.xml.gz
+# get all taxids available on ena taxonomy download (these are the ones also in ncbi taxdump)
+curl -s ftp://ftp.ebi.ac.uk/pub/databases/ena/taxonomy/taxonomy.xml.gz \
+| gunzip -c | grep "^<taxon" | perl -lne 'print $1 if /taxId="(\d+)"/' > ena-taxonomy.xml.taxids
 
-gunzip -c taxonomy.xml.gz | grep "^<taxon" | perl -lne 'print $1 if /taxId="(\d+)"/' > ena.taxonomy.xml.taxids
+# get ALL ena taxids from ena api (these include the ones NOT in ncbi taxdump)
 curl -s "https://www.ebi.ac.uk/ena/portal/api/search?result=taxon&query=tax_tree(2759)&limit=5000000" > resulttaxon.tax_tree2759.tsv
 
-# remove taxonomy.xml taxids
+# if an extra.curr.tsv file exists, get all the taxids from it and rename it extra.prev.taxids
+tail -n+2 resulttaxon.tax_tree2759.extra.curr.tsv | cut -f1 > resulttaxon.tax_tree2759.extra.prev.taxids
 
-fgrep -v -w -f ena.taxonomy.xml.taxids resulttaxon.tax_tree2759.tsv > resulttaxon.tax_tree2759.extra.curr.tsv
-wc -l resulttaxon.tax_tree2759.extra.curr.tsv
+# remove ena taxonomy download IDs and extra.prev.taxids from the ena api tsv
+cat ena-taxonomy.xml.taxids resulttaxon.tax_tree2759.extra.prev.taxids \
+| fgrep -v -w -f - resulttaxon.tax_tree2759.tsv > resulttaxon.tax_tree2759.extra.curr.tsv
 
-tail -n+2 resulttaxon.tax_tree2759.extra.curr.tsv \
-| while IFS=$'\t' read -r TAXID DESC
+# if prev jsonl exists, only keep those entries which are in current resulttaxon.tax_tree2759.tsv
+tail -n+2 resulttaxon.tax_tree2759.tsv \
+| cut -f1 \
+| perl -plne 's/(\d+)/"taxId" : "$1"/' \
+| fgrep -f - ena-taxonomy.extra.jsonl \
+> ena-taxonomy.extra.prev.jsonl
+
+# download the extra ENA jsons
+tail -n+2 resulttaxon.tax_tree2759.extra.curr.tsv | cut -f 1 \
+| while read -r TAXID
 do
   curl -s https://www.ebi.ac.uk/ena/taxonomy/rest/tax-id/$TAXID \
   | perl -lne '
@@ -21,10 +31,14 @@ do
     $jsonl .= $_;
     print $jsonl if /^}/
   '
-done > ena-taxonomy.extra.jsonl
+done > ena-taxonomy.extra.curr.jsonl
 
 # convert 3 part scientific name jsonl subspecies entries to include species as parent:
+cat ena-taxonomy.extra.curr.jsonl \
+| perl -plne 's/(.*scientificName\" : \"(\S+) (\S+) .*?\".*\"subspecies\".*?lineage\" : \".*?)( \".*)/$1 $2 $3;$4/' \
+| perl -lne 'print unless /scientificName\" : \"((\S+?)\s+(\S+?)\s+(.+?))\".*\"subspecies\"/ and (scalar split(/\s+/,$1) !=3 and $4!~/^(var|ssp|subsp)/)' \
+| grep -v -w 1676804 \
+| grep Eukaryota \
+| cat ena-taxonomy.extra.prev.jsonl - \
+> ena-taxonomy.extra.jsonl
 
-perl -i -plne 's/(.*scientificName\" : \"(\S+) (\S+) .*?\".*\"subspecies\".*?lineage\" : \".*?)( \".*)/$1 $2 $3;$4/' ena-taxonomy.extra.jsonl
-
-perl -lne 'print unless /scientificName\" : \"((\S+?)\s+(\S+?)\s+(.+?))\".*\"subspecies\"/ and (scalar split(/\s+/,$1) !=3 and $4!~/^(var|ssp|subsp)/)' ena-taxonomy.extra.jsonl | grep -v -w 1676804 | grep Eukaryota
