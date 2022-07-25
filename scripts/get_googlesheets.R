ef79135cccc6bf3d711d86dcc59416a2b4e1d6d9
Sujai Kumar <sujaikumar@gmail.com>
2022-07-25 14:03:41 +0530

    add aus spreadsheets to R script

--- scripts/get_googlesheets.R
+++ scripts/get_googlesheets.R
@@ -24,3 +24,23 @@ read_tsv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTU-En_URbYPtfyjBueQhn
   filter(!is.na(est_size_mb) | !is.na(length_mb)) -> tolqc
 
 write_tsv(tolqc, "./sources/genomesize_karyotype/DTOL_assembly_informatics_status_kmer_draft.tsv")
+
+# australian bioportal
+read_tsv("https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vS4iAxznp7djwBZE-m00ggKoVw8TZgxn19Lz1nYU20h_gYBARFd9ZS1zAjRpQlPE-68XK6zHKFfe4UA/pub?gid=890267489&output=tsv") %>%
+  clean_names() %>% remove_empty() %>% lapply(function(x) iconv(x, "latin1", "ASCII", sub="")) %>% as_tibble() %>%
+  select(bioplatforms_initiative, genus, species, subspecies, taxonomic_family_example_hominidae, ncbi_taxon_id, sequencing_status) %>%
+  mutate(bioplatforms_initiative = ifelse(bioplatforms_initiative == "Australian Grasslands", "AGI", bioplatforms_initiative)) %>%
+  mutate(bioplatforms_initiative = ifelse(bioplatforms_initiative %in% c("Wheat","Wine"), "GAP", bioplatforms_initiative)) %>%
+  filter(bioplatforms_initiative %in% c("OMG","ARG","TSI","GAP","GBR","AGI")) %>%
+  mutate(sequencing_status = ifelse(sequencing_status %in% c("complete","in progress"), "in_progress","")) %>%
+  mutate(in_progress = ifelse(sequencing_status == "in_progress",bioplatforms_initiative,"")) %>%
+  mutate(sample_acquired = ifelse(sequencing_status == "in_progress",bioplatforms_initiative,"")) %>%
+  mutate(sample_collected = ifelse(sequencing_status == "in_progress",bioplatforms_initiative,"")) %>%
+  mutate(target_status = "other_priority", long_list = bioplatforms_initiative, other_priority = bioplatforms_initiative) %>%
+  mutate(sequencing_status_omg = ifelse(bioplatforms_initiative == "OMG", sequencing_status,"")) %>%
+  mutate(sequencing_status_arg = ifelse(bioplatforms_initiative == "ARG", sequencing_status,"")) %>%
+  mutate(sequencing_status_tsi = ifelse(bioplatforms_initiative == "TSI", sequencing_status,"")) %>%
+  mutate(sequencing_status_gap = ifelse(bioplatforms_initiative == "GAP", sequencing_status,"")) %>%
+  mutate(sequencing_status_gbr = ifelse(bioplatforms_initiative == "GBR", sequencing_status,"")) %>%
+  mutate(sequencing_status_agi = ifelse(bioplatforms_initiative == "AGI", sequencing_status,"")) %>%
+  write_tsv("./sources/status_lists/Aus_Genome_status_Bioplatforms.tsv")
