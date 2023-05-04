87bbee92bdc19e8ecada407fa46b0a7a92b69166
Sujai Kumar <sujaikumar@gmail.com>
2023-05-04 17:30:34 +0100

    refactor api to tsv scripts

--- scripts/api_to_tsv.py
+++ scripts/api_to_tsv.py
@@ -1,19 +1,28 @@
 import api_tools as at
 import api_config as cfg
+import sys
 
+# at.get_from_source(
+#     cfg.vgl_url_opener, 
+#     cfg.vgl_hub_count_handler, 
+#     cfg.vgl_row_handler, 
+#     cfg.vgl_fieldnames,
+#     cfg.vgl_output_filename
+# )
+
+# at.get_from_source(
+#     cfg.nhm_url_opener, 
+#     cfg.nhm_api_count_handler, 
+#     cfg.nhm_row_handler, 
+#     cfg.nhm_fieldnames,
+#     cfg.nhm_output_filename
+# )
 
 at.get_from_source(
-    cfg.vgl_url, 
-    cfg.vgl_hub_count_handler, 
-    cfg.vgl_row_handler, 
-    cfg.vgl_fieldnames,
-    cfg.vgl_output_filename
-)
-
-at.get_from_source(
-    cfg.nhm_url, 
-    cfg.nhm_api_count_handler, 
-    cfg.nhm_row_handler, 
-    cfg.nhm_fieldnames,
-    cfg.nhm_output_filename
+    cfg.sts_url_opener, 
+    cfg.sts_api_count_handler, 
+    cfg.sts_row_handler, 
+    cfg.sts_fieldnames,
+    cfg.sts_output_filename,
+    token=sys.argv[1],
 )
