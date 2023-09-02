49e7f690b5734551ff360d253bc35cfeab3abc29
ccaio <cibele.caio@gmail.com>
2023-09-02 18:12:20 +0100

    Allow url slugs for NHM from fieldnames in import

--- scripts/api_tools.py
+++ scripts/api_tools.py
@@ -1,14 +1,19 @@
-import requests
+# import requests
 import csv
 
-def get_from_source(url_opener, count_handler, row_handler, fieldnames, output_filename, token=None):
+
+def get_from_source(
+    url_opener, count_handler, row_handler, fieldnames, output_filename, token=None
+):
     r = url_opener(token=token)
     r_text = r.text
     result_count = count_handler(r_text)
-    print(f'count is {result_count}')
-    rows = row_handler(r_text=r_text, result_count=result_count, fieldnames=fieldnames, token=token)
-    
-    with open(output_filename, 'w') as output_file:
-        writer = csv.writer(output_file, delimiter='\t', lineterminator='\n')
+    print(f"count is {result_count}")
+    rows = row_handler(
+        r_text=r_text, result_count=result_count, fieldnames=fieldnames, token=token
+    )
+
+    with open(output_filename, "w") as output_file:
+        writer = csv.writer(output_file, delimiter="\t", lineterminator="\n")
         writer.writerow(fieldnames)
         writer.writerows(rows)
