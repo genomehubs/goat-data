8ba453c6b90c4ecff367936d064311c70f9546c7
Richard Challis <richard.challis@sanger.ac.uk>
2023-10-20 13:12:52 +0100

    _

---
+++ scripts/update-resources/get_ena_taxonomy_extra.bash
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+TAXROOT=$1
+if [ -z $TAXROOT ]; then
+    TAXROOT=2759
+fi
+
+# get all taxids available on ena taxonomy download (these are the ones also in ncbi taxdump)
+curl -L https://ftp.ebi.ac.uk/pub/databases/ena/taxonomy/taxonomy.xml.gz \
+| gunzip -c | grep "^<taxon" | perl -lne 'print $1 if /taxId="(\d+)"/' > ena-taxonomy.xml.taxids || exit 0
+
+if [ $(stat -c %s ena-taxonomy.xml.taxids) -lt 10000000 ]; then exit 0; fi
+
+# get ALL ena taxids from ena api (these include the ones NOT in ncbi taxdump)
+curl -L "https://www.ebi.ac.uk/ena/portal/api/search?result=taxon&query=tax_tree($TAXROOT)&limit=10000000" > resulttaxon.tax_tree$TAXROOT.taxid_desc || exit 1
+
+if [ $(stat -c %s resulttaxon.tax_tree$TAXROOT.taxid_desc) -lt 21 ]; then exit 0; fi
+
+# taxids may be in column 1 or 2 - check which before cutting!
+if [[ "$(head -n 1 resulttaxon.tax_tree$TAXROOT.taxid_desc | cut -f1)" == tax_id ]]; then
+  cut -f1 resulttaxon.tax_tree$TAXROOT.taxid_desc > resulttaxon.tax_tree$TAXROOT.taxids
+elif [[ "$(head -n 1 resulttaxon.tax_tree$TAXROOT.taxid_desc | cut -f2)" == tax_id ]]; then
+  cut -f2 resulttaxon.tax_tree$TAXROOT.taxid_desc > resulttaxon.tax_tree$TAXROOT.taxids
+else
+  exit 1
+fi
+
+# if prev extra jsonl exists, gunzip it first
+gunzip ena-taxonomy.extra.$TAXROOT.jsonl.gz 2>/dev/null || touch ena-taxonomy.extra.$TAXROOT.jsonl
+
+# then only keep those entries which are in current resulttaxon.tax_tree$TAXID.tsv
+tail -n+2 resulttaxon.tax_tree$TAXROOT.taxids \
+| perl -plne 's/(\d+)/"taxId" : "$1"/' \
+| fgrep -f - ena-taxonomy.extra.$TAXROOT.jsonl \
+> ena-taxonomy.extra.$TAXROOT.prev.jsonl
+
+# get taxids from ena-taxonomy.extra.prev.jsonl (these don't need to be downloaded again)
+perl -plne 's/.*\"taxId\" : "(\d+)\".*/$1/' ena-taxonomy.extra.$TAXROOT.prev.jsonl > ena-taxonomy.extra.$TAXROOT.prev.taxids
+
+# remove prev ena jsonl, ftp taxonomy download IDs, and extra.prev.taxids from the ena api tsv
+cat ena-taxonomy.extra.$TAXROOT.prev.taxids ena-taxonomy.xml.taxids \
+| fgrep -v -w -f - resulttaxon.tax_tree$TAXROOT.taxids > resulttaxon.tax_tree$TAXROOT.extra.curr.taxids
+
+# download the extra ENA jsons
+tail -n+2 resulttaxon.tax_tree$TAXROOT.extra.curr.taxids \
+| while read -r TAXID
+do
+  echo https://www.ebi.ac.uk/ena/taxonomy/rest/tax-id/$TAXID
+  curl -s https://www.ebi.ac.uk/ena/taxonomy/rest/tax-id/$TAXID \
+  | perl -lne '
+    $jsonl = "" if /^{/;
+    $jsonl .= $_;
+    print $jsonl if /^}/
+  '
+done > ena-taxonomy.extra.$TAXROOT.curr.jsonl
+
+# cat the prev and curr jsonl, but sort by length of lineage:
+cat ena-taxonomy.extra.$TAXROOT.prev.jsonl ena-taxonomy.extra.$TAXROOT.curr.jsonl \
+| perl -lne '
+{
+  $jsonl = $_;
+  $lineage = $1 if /"lineage" : "(.+?)"/;
+  $lineage_count = ($lineage =~ s/;/;/g);
+  $jsonl_hash{$jsonl} = $lineage_count;
+}
+END
+{
+  foreach my $jsonl (sort { $jsonl_hash{$a} <=> $jsonl_hash{$b} } keys %jsonl_hash) {
+    print $jsonl;
+  }
+}' > ena-taxonomy.extra.$TAXROOT.jsonl
+
+gzip ena-taxonomy.extra.$TAXROOT.jsonl
