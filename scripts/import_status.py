709feca7f0f26c65e03374eda27da4c580447b54
Richard Challis <richard.challis@sanger.ac.uk>
2023-10-20 16:02:04 +0100

    _

--- scripts/import_status.py
+++ scripts/import_status.py
@@ -8,12 +8,16 @@ private_tsv = isl.open_private_tsv(sys.argv[1])
 private_tsv = private_tsv.reset_index()  # make sure indexes pair with number of rows
 
 for index, row in private_tsv.iterrows():
-    print(row["project_acronym"]
-        #   , str(row["published_url"])
-          , int(row["start_header_line"]))
+    print(
+        row["project_acronym"],
+        int(row["start_header_line"]),
+    )
     try:
-        isl.processing_schema_2_5_lists(row["project_acronym"]
-                                       , str(row["published_url"])
-                                       , int(row["start_header_line"]))
-    except:
-        print('something has gone wrong: ', row["project_acronym"])
+        isl.processing_schema_2_5_lists(
+            row["project_acronym"],
+            str(row["published_url"]),
+            int(row["start_header_line"]),
+            sys.argv[2],
+        )
+    except Exception:
+        print("something has gone wrong: ", row["project_acronym"])
--- scripts/jgi_to_tsv.py
+++ scripts/jgi_to_tsv.py
@@ -1,43 +1,62 @@
 #!/usr/bin/env python
 
-import json
 import csv
 import sys
 import requests
 
-url='https://gold-ws.jgi.doe.gov'
-OFFLINE_TOKEN=sys.argv[1]
-ACCESS_TOKEN = requests.get(url + '/exchange?offlineToken=' + OFFLINE_TOKEN).content.decode()
+url = "https://gold-ws.jgi.doe.gov"
+OFFLINE_TOKEN = sys.argv[1]
+ACCESS_TOKEN = requests.get(
+    url + "/exchange?offlineToken=" + OFFLINE_TOKEN
+).content.decode()
 
 headers = {"Authorization": "Bearer " + ACCESS_TOKEN, "Accept": "application/json"}
 
-r = requests.get(url + '/api/v1/organisms?studyGoldId=Gs0000001', headers=headers).json()
+r = requests.get(
+    url + "/api/v1/organisms?studyGoldId=Gs0000001", headers=headers
+).json()
 org2taxid = {}
 
 for organism in r:
-  org2taxid[organism['organismGoldId']] = organism['ncbiTaxId']
+    org2taxid[organism["organismGoldId"]] = organism["ncbiTaxId"]
 
-r = requests.get(url + '/api/v1/projects?studyGoldId=Gs0000001', headers=headers).json()
+r = requests.get(url + "/api/v1/projects?studyGoldId=Gs0000001", headers=headers).json()
 
 fieldnames = [
-  "projectGoldId", "projectName", "legacyGoldId", "studyGoldId",
-  "biosampleGoldId", "organismGoldId", "itsProposalId", "itsSpid",
-  "itsSampleId", "pmoProjectId", "gptsProposalId",
-  "ncbiBioProjectAccession", "ncbiBioSampleAccession",
-  "projectStatus", "sequencingStatus",
-  "jgiFundingProgram", "jgiFundingYear", "hmpId",
-  "modDate", "addDate", "sequencingStrategy",
-  "sequencingCenters", "seqMethod",
-  "genomePublications", "otherPublications",
-  "sraExperimentIds"
-  ]
+    "projectGoldId",
+    "projectName",
+    "legacyGoldId",
+    "studyGoldId",
+    "biosampleGoldId",
+    "organismGoldId",
+    "itsProposalId",
+    "itsSpid",
+    "itsSampleId",
+    "pmoProjectId",
+    "gptsProposalId",
+    "ncbiBioProjectAccession",
+    "ncbiBioSampleAccession",
+    "projectStatus",
+    "sequencingStatus",
+    "jgiFundingProgram",
+    "jgiFundingYear",
+    "hmpId",
+    "modDate",
+    "addDate",
+    "sequencingStrategy",
+    "sequencingCenters",
+    "seqMethod",
+    "genomePublications",
+    "otherPublications",
+    "sraExperimentIds",
+]
 
 output_file = sys.stdout
 
-writer = csv.writer(output_file, delimiter='\t', lineterminator='\n')
-writer.writerow(fieldnames + ['ncbiTaxId'])
+writer = csv.writer(output_file, delimiter="\t", lineterminator="\n")
+writer.writerow(fieldnames + ["ncbiTaxId"])
 
 for project in r:
-  d = [project.get(f) for f in fieldnames]
-  if project["sequencingStrategy"]=="Whole Genome Sequencing":
-    writer.writerow(d + [org2taxid[project['organismGoldId']]])
+    d = [project.get(f) for f in fieldnames]
+    if project["sequencingStrategy"] == "Whole Genome Sequencing":
+        writer.writerow(d + [org2taxid[project["organismGoldId"]]])
