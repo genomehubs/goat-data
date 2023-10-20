05d4cedcd4646244fda213e49fe2b90828e54414
Richard Challis <richard.challis@sanger.ac.uk>
2023-10-20 13:22:39 +0100

    _

--- scripts/update-resources/get_ena_taxonomy_extra.bash
+++ scripts/update-resources/get_ena_taxonomy_extra.bash
@@ -6,15 +6,23 @@ if [ -z $TAXROOT ]; then
 fi
 
 # get all taxids available on ena taxonomy download (these are the ones also in ncbi taxdump)
-curl -L https://ftp.ebi.ac.uk/pub/databases/ena/taxonomy/taxonomy.xml.gz \
+curl -Ls https://ftp.ebi.ac.uk/pub/databases/ena/taxonomy/taxonomy.xml.gz \
 | gunzip -c | grep "^<taxon" | perl -lne 'print $1 if /taxId="(\d+)"/' > ena-taxonomy.xml.taxids || exit 0
 
-if [ $(stat -c %s ena-taxonomy.xml.taxids) -lt 10000000 ]; then exit 0; fi
+if [ $(stat -c %s ena-taxonomy.xml.taxids) -lt 10000000 ]; then 
+  echo "Unable to fetch taxonomy"
+  exit 0;
+fi
 
 # get ALL ena taxids from ena api (these include the ones NOT in ncbi taxdump)
-curl -L "https://www.ebi.ac.uk/ena/portal/api/search?result=taxon&query=tax_tree($TAXROOT)&limit=10000000" > resulttaxon.tax_tree$TAXROOT.taxid_desc || exit 1
+curl -Ls "https://www.ebi.ac.uk/ena/portal/api/search?result=taxon&query=tax_tree($TAXROOT)&limit=10000000" > resulttaxon.tax_tree$TAXROOT.taxid_desc || \
+echo "Unable to fetch taxids" && \
+exit 1
 
-if [ $(stat -c %s resulttaxon.tax_tree$TAXROOT.taxid_desc) -lt 21 ]; then exit 0; fi
+if [ $(stat -c %s resulttaxon.tax_tree$TAXROOT.taxid_desc) -lt 21 ]; then 
+  echo "No taxids in file"
+  exit 0;
+fi
 
 # taxids may be in column 1 or 2 - check which before cutting!
 if [[ "$(head -n 1 resulttaxon.tax_tree$TAXROOT.taxid_desc | cut -f1)" == tax_id ]]; then
@@ -22,11 +30,15 @@ if [[ "$(head -n 1 resulttaxon.tax_tree$TAXROOT.taxid_desc | cut -f1)" == tax_id
 elif [[ "$(head -n 1 resulttaxon.tax_tree$TAXROOT.taxid_desc | cut -f2)" == tax_id ]]; then
   cut -f2 resulttaxon.tax_tree$TAXROOT.taxid_desc > resulttaxon.tax_tree$TAXROOT.taxids
 else
+  echo "ERROR: Taxid list is not a valid format"
+  head -n 2 resulttaxon.tax_tree$TAXROOT.taxid_desc
   exit 1
 fi
 
 # if prev extra jsonl exists, gunzip it first
-gunzip ena-taxonomy.extra.$TAXROOT.jsonl.gz 2>/dev/null || touch ena-taxonomy.extra.$TAXROOT.jsonl
+gunzip ena-taxonomy.extra.$TAXROOT.jsonl.gz 2>/dev/null || \
+echo "No previous jsonl file found" && \
+touch ena-taxonomy.extra.$TAXROOT.jsonl
 
 # then only keep those entries which are in current resulttaxon.tax_tree$TAXID.tsv
 tail -n+2 resulttaxon.tax_tree$TAXROOT.taxids \
