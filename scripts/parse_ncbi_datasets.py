4f9243814d25b4ec32777729a031f924dac0c1b3
Richard Challis <richard.challis@sanger.ac.uk>
2024-06-20 09:03:11 +0100

    update

--- scripts/parse_ncbi_datasets.py
+++ scripts/parse_ncbi_datasets.py
@@ -576,7 +576,7 @@ def main():
             previous_row = previous_parsed[accession]
             if data["assemblyInfo"]["releaseDate"] == previous_row["releaseDate"]:
                 row = previous_row
-                print(row)
+                print(previous_features[accession])
                 if (
                     args.features is not None
                     and accession in previous_features
