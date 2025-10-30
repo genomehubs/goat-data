8f1aa01f23f9481b317a6948ee991510175b894f
Cibele Sotero-Caio <cs43@sanger.ac.uk>
2025-10-30 18:45:38 +0000

    add helper for api fetching, update nhm headers and make sure output directory exists

--- scripts/api/api_to_tsv.py
+++ scripts/api/api_to_tsv.py
@@ -3,29 +3,49 @@
 Will generate .tsv files from api-retrieved data for GoaT import.
 """
 
-
 import sys
-import api_tools as at
+
 import api_config as cfg
+import api_tools as at
+
+"""try to access each API 3 times in case of failure or timeout"""
 
 
-at.get_from_source(
+def access_api_with_retries(
+    url_opener, api_count_handler, row_handler, fieldnames, output_filename, token=None
+):
+    for i in range(3):
+        try:
+            at.get_from_source(
+                url_opener,
+                api_count_handler,
+                row_handler,
+                fieldnames,
+                output_filename,
+                token=token,
+            )
+            break  # Exit the loop if successful
+        except Exception as e:
+            print(f"Error occurred while accessing API: {e}")
+            if i == 2:
+                print(f"Max retries for {output_filename} reached. Exiting.")
+
+
+access_api_with_retries(
     cfg.vgl_url_opener,
     cfg.vgl_hub_count_handler,
     cfg.vgl_row_handler,
     cfg.vgl_fieldnames,
     f"{sys.argv[1]}/{cfg.vgl_output_filename}",
 )
-
-at.get_from_source(
+access_api_with_retries(
     cfg.nhm_url_opener,
     cfg.nhm_api_count_handler,
     cfg.nhm_row_handler,
     cfg.nhm_fieldnames,
     f"{sys.argv[1]}/{cfg.nhm_output_filename}",
 )
-
-at.get_from_source(
+access_api_with_retries(
     cfg.sts_url_opener,
     cfg.sts_api_count_handler,
     cfg.sts_row_handler,
